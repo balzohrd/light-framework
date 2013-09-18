6d7bd6a1a241ac560c2c2ff2fe9e3f400e4e17e6
Myles O'Connor <mocon@guidance.com>
2013-09-18 13:07:30 -0700

    Updates to responsive header

--- inc/header.php
+++ inc/header.php
@@ -2,19 +2,19 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-1">
-				<span style="float:left;"><img src="img/sloth.jpg" /></span>
+				<span style="float:left;"><img src="img/sloth.png" /></span>
 				<span style="float:right;">
 					<ul class="h-p h-t">
-						<li><a href="#"><i class="icon-th-large"></i> Home</a></li>
+						<li><a href="#"><i class="icon-th"></i> Home</a></li>
 						<li><a href="#"><i class="icon-user"></i> Profile</a></li>
 						<li><a href="#"><i class="icon-cogs"></i> Account</a></li>
 						<li><a href="#"><i class="icon-signin"></i> Login</a></li>
 					</ul>
 					<ul class="h-d">
-						<li><a href="#"><i class="icon-th-large"></i></a></li>
+						<li><a href="#"><i class="icon-th"></i></a></li>
 						<li><a href="#"><i class="icon-user"></i></a></li>
 						<li><a href="#"><i class="icon-cogs"></i></a></li>
-						<li><a href="#"><i class="icon-signin"></i> Login</a></li>
+						<li><a href="#"><i class="icon-signin"></i><span id="login-text"> Login</span></a></li>
 					</ul>
 				</span>
 				<div class="clearfix"></div>
